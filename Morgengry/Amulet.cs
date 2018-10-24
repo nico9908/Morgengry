a295e05a040074c78b2c105fe17f1b2ed3eb0ec9
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2018-10-24 09:06:58 +0200

    Tilføjet Book, Amulet class og enum i Amulet class.

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class Amulet
+    {
+        string itemid;
+        string design;
+        enum level
+        {
+            low,
+            medium,
+            high
+        }
+    }
+}
